e6ff09609abfe6f0999c9b27cc1a5c34f3529370
dylanesco23 <73003649+dylanesco23@users.noreply.github.com>
2025-10-03 22:41:56 -0500

    Update program.py

--- program.py
+++ program.py
@@ -1,1 +1,5 @@
-print("hello")
+# Simple greeting program
+name = input("What's your name? ")
+birthday = input("When is your birthday? (MM/DD) ")
+
+print(f"Hello, {name}! Happy upcoming birthday on {birthday}!")
